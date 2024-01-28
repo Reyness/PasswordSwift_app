59d2b501c95c937fe90d8d7e987dd6fc5ff1223f
Antoni Coll Reynés <acollreynes@hotmail.com>
2024-01-28 13:37:38 +0100

    solución fallo número de caracteres

--- js/password-generator.js
+++ js/password-generator.js
@@ -1,10 +1,15 @@
+// Función principal para generar una contraseña
 function generatePassword() {
+  // Obtener la longitud deseada de la contraseña
   const length = parseInt(document.querySelector(".numbers").value);
+
+  // Obtener las opciones de configuración del usuario
   const useUppercase = document.getElementById("uppercase").checked;
   const useLowercase = document.getElementById("lowercase").checked;
   const useNumbers = document.getElementById("numbers").checked;
   const useSymbols = document.getElementById("symbols").checked;
 
+  // Generar el conjunto de caracteres basado en las opciones del usuario
   const charset = generateCharset(
     useUppercase,
     useLowercase,
@@ -12,6 +17,7 @@ function generatePassword() {
     useSymbols
   );
 
+  // Inicializar la contraseña y el contador de categorías utilizadas
   let password = "";
   let categoriesCount = 0;
 
@@ -35,56 +41,21 @@ function generatePassword() {
 
   // Completar el resto de la contraseña con caracteres aleatorios del conjunto
   for (let i = categoriesCount; i < length; i++) {
-    const randomChar = getRandomChar(charset);
-
-    // Evitar solo letras o solo números
-    if ((useUppercase || useLowercase) && /^\d+$/.test(randomChar)) {
-      i--; // Repetir la iteración si se generó un número y solo se permiten letras
-      continue;
-    }
-    if (useNumbers && /^[a-zA-Z]+$/.test(randomChar)) {
-      i--; // Repetir la iteración si se generó una letra y solo se permiten números
-      continue;
-    }
-
-    // Evitar caracteres repetidos
-    if (password.includes(randomChar)) {
-      i--; // Repetir la iteración si el carácter generado ya está en la contraseña
-      continue;
-    }
-
-    // Evitar letras mayúsculas consecutivas
-    if (useUppercase && password.slice(-1).toUpperCase() === randomChar) {
-      i--; // Repetir la iteración si se generó una letra mayúscula consecutiva
-      continue;
-    }
-
-    // Evitar letras minúsculas consecutivas
-    if (useLowercase && password.slice(-1).toLowerCase() === randomChar) {
-      i--; // Repetir la iteración si se generó una letra minúscula consecutiva
-      continue;
-    }
-
-    // Evitar números consecutivos
-    if (useNumbers && /\d/.test(randomChar) && /\d/.test(password.slice(-1))) {
-      i--; // Repetir la iteración si se generó un número consecutivo
-      continue;
-    }
-
-    // Otras condiciones a evitar (sencuencia de letras, secuencia de números) pueden agregarse según sea necesario.
-
-    // Agregar el carácter a la contraseña
-    password += randomChar;
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
   }
 
+  // Mostrar la contraseña generada en el campo de entrada
   document.querySelector(".pass-generate").value = password;
 }
 
+// Función para obtener un carácter aleatorio de un conjunto dado
 function getRandomChar(charset) {
   const randomIndex = Math.floor(Math.random() * charset.length);
   return charset[randomIndex];
 }
 
+// Función para generar el conjunto de caracteres basado en las opciones del usuario
 function generateCharset(useUppercase, useLowercase, useNumbers, useSymbols) {
   let charset = "";
   if (useUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
